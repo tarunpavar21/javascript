0686babcd528eb29484a92330a8df56d2d06a06a
TARUN <tarunpawar819@gmail.com>
2025-01-15 18:20:31 +0530

    conditional statement

---
+++ conditon.js
@@ -0,0 +1,50 @@
+// // if statement 
+
+
+// let age = 23;
+
+// if (age >= 18)
+// {
+//     console.log("you can vote");
+// }
+// if (age < 18 )
+// {
+//     console.log("you can not vote")
+// }
+
+
+// // if else statement 
+
+// let mod = "dark";
+// let color;
+// if (mod === "dark"){
+//     color = "black";
+// }
+// else{
+//     color = "white";
+// }
+// console.log(color);
+
+// else if statement 
+
+// let mod = "blue";
+// let color;
+
+// if (mod === "dark"){
+//     color = "black";
+// }else if (mod === "blue"){
+//     color = "blue";
+// }else if (mod === "pink"){
+//     color = "pink";
+// }else {
+//     color = "white";
+// }
+// console.log(color);
+
+
+
+// ternary Operators
+let age = 22;
+
+let result = age >= 19 ? "adult" : "not adult";
+console.log(result);
