f5f2e10cbe0fc639e8d7730dda5a814e254be49b
TARUN <tarunpawar819@gmail.com>
2025-01-15 17:30:23 +0530

    operators in java script

---
+++ operators.js
@@ -0,0 +1,80 @@
+//arithmatic operators
+
+// let a = 5;
+// let b = 2;
+
+
+// console.log("a =",a, "& b =",b)
+// console.log("a+b = ", a+b);
+// console.log("a-b = ", a-b);
+// console.log("a*b = ", a*b);
+// console.log("a/b = ", a/b);
+// console.log("a%b = ", a%b);
+// console.log("a exponantions b = ", a**b);
+
+// unary Operators
+
+// let a = 5;
+// let b = 2;
+
+
+// console.log("a =",a, "& b =",b)
+// a++;
+// console.log("a = ",a);
+
+// b--
+// console.log("b = ",b);
+
+//asignment operators
+
+// let a = 5;
+// let b = 2;
+
+// a += 4; // a = a + 4
+// console.log("a = ",a);
+// a /= 4; // a = a / 4
+// console.log("a = ",a);
+// a -= 4; // a = a - 4
+// console.log("a = ",a);
+// a *= 4; // a = a * 4
+// console.log("a = ",a);
+// a %= 4; // a = a % 4
+// console.log("a = ",a);
+// a **= 4; // a = a ** 4
+// console.log("a = ",a);
+
+
+// comparison Operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a == b", a==b);
+// console.log("a != b", a!=b);
+// console.log("a === b", a===b);
+// console.log("a !== b", a!==b);
+// console.log("a >= b", a>=b);
+// console.log("a <= b", a<=b);
+// console.log("a > b", a > b);
+// console.log("a < b", a <b);
+
+// logial Operators
+
+let a = 5;
+let b = 2;
+
+let cond1 = a > b;
+let cond2 = b < a;
+
+console.log("cond1 && cond2 =", cond1 && cond2);
+console.log("cond1 || cond2 =", cond1 || cond2);
+console.log("! cond1 =", !(cond1));
+
+
+
+
+
+
+
+
+
