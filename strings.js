ffebe2974b0f4f464c0ce943c4ecc531283ee5c7
TARUN <tarunpawar819@gmail.com>
2025-02-12 15:01:25 +0530

    string in js

---
+++ strings.js
@@ -0,0 +1,24 @@
+
+let name = "TArun";
+console.log(name);
+
+console.log(typeof(name));
+
+
+//template literals
+
+let boy1 = "pramod"
+let boy2 = "nimesh"
+
+// pramod is friend of nimesh
+
+let sentence = `${boy1} is friend of ${boy2}`
+console.log(sentence);
+
+// escaps sequences characters
+
+let Name = 'Tarun\'s Friend';
+
+console.log(Name);
+
+
