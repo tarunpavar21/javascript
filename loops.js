d027adfcbf4f3c354952f017cfef9f83766d61dd
TARUN <tarunpawar819@gmail.com>
2025-02-12 11:59:20 +0530

    loops in js

---
+++ loops.js
@@ -0,0 +1,34 @@
+// for loop
+// let i;
+// for(i=1; i<=10; i++){
+//     console.log("i= ",i);
+// }
+
+// calculate sum of n number 
+// let sum = 0;
+// let n = 10;
+
+// for(let i = 1; i<=n; i++){
+//     sum = sum+i;
+    
+// }
+// console.log("sum= ",sum);
+
+
+// while loops 
+
+// let i = 1;
+
+// while(i<=5){
+//     console.log("i= ",i);
+//     i++
+// }
+
+
+// do while loop
+
+let i = 10;
+do{
+    console.log("hellow world");
+    i++;
+}while(i<=10)
