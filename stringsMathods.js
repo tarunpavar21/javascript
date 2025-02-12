4a45469f4fca39b3281759c5962d40f1452981e0
TARUN <tarunpawar819@gmail.com>
2025-02-12 15:56:52 +0530

    strings mathods

---
+++ stringsMathods.js
@@ -0,0 +1,24 @@
+
+
+let name = "tarun"
+// console.log(name.length);
+
+// console.log(name.toUpperCase());
+
+// console.log(name.toLowerCase());
+
+// // slice of string
+
+// console.log(name.slice(2,4));
+
+// //replace string 
+// console.log(name.replace("tar","Aar"));
+
+let friend = "naman";
+
+console.log(name.concat(" is friend of " , friend," ok"));
+
+
+let friend2 = "     meena     ";
+console.log(friend2);
+console.log(friend2.trim());
